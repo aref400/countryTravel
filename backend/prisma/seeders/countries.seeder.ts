a40a41033e41a9809040aa771b9ef0af2c5f4422
Lucas <lucas-lafourcade@live.com>
2026-02-19 14:15:04 +0100

    ajout de la seed prisma

---
+++ backend/prisma/seeders/countries.seeder.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "../../generated/prisma/client";
+
+export async function seedCountries(prisma: PrismaClient){
+    //fetch api Rest Countries
+    const response = await fetch("https://restcountries.com/v3.1/all?fields=cca2,name,region,flags");
+    const countries = await response.json();
+
+    const TARGET_ISO_CODES = [
+  'FR', // France
+  'IE', // Irlande
+  'AT', // Autriche
+  'HU', // Hongrie
+  'HR', // Croatie
+  'ES', // Espagne
+  'NO', // Norvège
+  'SE', // Suède
+  'JP', // Japon
+  'CN', // Chine
+  'KR', // Corée du Sud
+  'TH', // Thailande
+  'NP', // Népal
+  'PA', // Panama
+  'CO', // Colombie
+  'BR', // Brésil
+  'AR', // Argentine
+  'BO', // Bolivie
+  'TN', // Tunisie
+  'MA'  // Maroc
+];
+const filteredCountries = countries.filter((country: any) => TARGET_ISO_CODES.includes(country.cca2));
+
+for (const country of filteredCountries) {
+    await prisma.country.upsert({
+        where :{ isoCode: country.cca2 },
+        create: {
+            isoCode: country.cca2,
+            name: country.name.common,
+            region: country.region,
+            flagUrl: country.flags.png
+        },
+        update: {
+            name: country.name.common,
+            region: country.region,
+            flagUrl: country.flags.png
+        }
+    })
+}
+}
