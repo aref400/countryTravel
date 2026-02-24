5bc6a3ee7e89257f00ee0484822ed3386958d6c2
Lucas <lucas-lafourcade@live.com>
2026-02-24 11:28:51 +0100

    modif appel prismaClient

--- backend/prisma/seeders/profiles.seeder.ts
+++ backend/prisma/seeders/profiles.seeder.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { countriesProfiles } from '../data/countries-profiles';
 
 export async function seedProfiles(prisma: PrismaClient) {
