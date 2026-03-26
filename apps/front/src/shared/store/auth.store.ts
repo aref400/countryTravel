27f6f8cd6d08c413efba5cf2d91c001afb764932
Lucas <lucas-lafourcade@live.com>
2026-03-26 14:23:31 +0100

    [CT-02] Ajout du provider, du router, d'axios, et création du store du token + modif architecture

---
+++ apps/front/src/shared/store/auth.store.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface AuthState {
+  token: string | null;
+  setToken(token: string | null): void;
+  logout(): void;
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      token: null,
+      setToken: (token) => set({ token }),
+      logout: () => {
+        set({ token: null });
+        window.location.href = "/login";
+      },
+    }),
+    {
+      name: "auth-storage",
+    },
+  ),
+);
